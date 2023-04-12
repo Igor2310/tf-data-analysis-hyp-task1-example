4637b88a4af0428a6f5c6241f35e42170defddc5
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-13 00:14:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,6 @@
-import numpy
-import statsmodels.stats.proportion as p
-from scipy.stats import chi2
-from scipy.stats import laplace, norm
-
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
 
 chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
 
@@ -10,6 +8,10 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    v = (x_success / x_cnt - y_success / y_cnt) * numpy.sqrt((x_cnt + y_cnt) * x_cnt * y_cnt / ((x_success + y_success) * (x_cnt + y_cnt - x_success - y_success)))
-    q = norm.ppf(0.03)
-    return v <= q
+    alpha = 0.03
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    se = ((p1*(1-p1))/x_cnt + (p2*(1-p2))/y_cnt)**0.5
+    z = (p1 - p2) / se
+    critical_value = abs(norm.ppf(alpha/2))
+    return abs(z) > critical_value
